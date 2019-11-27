3bdcccdb608d429432e80a6be8cb9131492e029e
Urska Pangerc <urska.pangerc@gmail.com>
2019-11-27 11:07:47 +0100

    upgrade changes due to skd 28 and react native 59

--- android/src/main/java/com/abelium/braintreeccform/CreditCardShadowNode.java
+++ android/src/main/java/com/abelium/braintreeccform/CreditCardShadowNode.java
@@ -1,12 +1,12 @@
 package com.abelium.braintreeccform;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.View;
 
-import com.facebook.csslayout.CSSMeasureMode;
-import com.facebook.csslayout.CSSNodeAPI;
-import com.facebook.csslayout.MeasureOutput;
+import com.facebook.yoga.YogaMeasureMode;
+import com.facebook.yoga.YogaMeasureFunction;
+import com.facebook.yoga.YogaNode;
+import com.facebook.yoga.YogaMeasureOutput;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.uimanager.LayoutShadowNode;
 import com.facebook.react.uimanager.annotations.ReactProp;
@@ -32,7 +32,7 @@ public class CreditCardShadowNode extends LayoutShadowNode
     measureControl.setHidePayButton(hidePayButton);
   }
 
-  public static int translateMeasureSpec(CSSMeasureMode mode) {
+  public static int translateMeasureSpec(YogaMeasureMode mode) {
     switch ( mode ) {
       case AT_MOST: return View.MeasureSpec.AT_MOST;
       case EXACTLY: return View.MeasureSpec.EXACTLY;
@@ -41,16 +41,14 @@ public class CreditCardShadowNode extends LayoutShadowNode
     return View.MeasureSpec.UNSPECIFIED;
   }
 
-  public final MeasureFunction measureFunction = new MeasureFunction() {
+  public final YogaMeasureFunction measureFunction = new YogaMeasureFunction() {
     @Override
-    public long measure(CSSNodeAPI node, float width, CSSMeasureMode widthMode, float height, CSSMeasureMode heightMode) {
-      // Log.i(CreditCardControlManager.TAG, String.format("MEASURE %s, %s %s, %s %s", node, width, widthMode, height, heightMode));
+    public long measure(YogaNode node, float width, YogaMeasureMode widthMode, float height, YogaMeasureMode heightMode) {
       //noinspection WrongConstant
       measureControl.measure(
         View.MeasureSpec.makeMeasureSpec((int) width, translateMeasureSpec(widthMode)),
         View.MeasureSpec.makeMeasureSpec((int) height, translateMeasureSpec(heightMode)));
-      // Log.i(CreditCardControlManager.TAG, String.format("MEASURE RESULT: width=%s height=%s", measureControl.getMeasuredWidth(), measureControl.getMeasuredHeight()));
-      return MeasureOutput.make(measureControl.getMeasuredWidth(), measureControl.getMeasuredHeight());
+      return YogaMeasureOutput.make(measureControl.getMeasuredWidth(), measureControl.getMeasuredHeight());
     }
   };
 }
